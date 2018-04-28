21e658e24564e110c4ddc709b45e9b104add4730
BohdanRadchuk <shpionuse@gmail.com>
2018-04-28 11:35:38 +0300

    security try

---
+++ src/main/java/com/company/service/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.company.service;
+
+import com.company.model.Role;
+import com.company.model.User;
+import com.company.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null){
+            throw new UsernameNotFoundException(email);
+        }
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Role role: user.getRoles()
+             ) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getRole()));
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), grantedAuthorities);
+    }
+}
